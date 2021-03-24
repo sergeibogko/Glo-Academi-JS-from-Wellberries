e6933f20f14fd6ed0575c3e0fbc84e7ca15e05e3
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2021-03-24 18:06:45 +0200

    HW2

--- 5f4be3f1142a28edc2de77669a33b477/js/main.js
+++ 5f4be3f1142a28edc2de77669a33b477/js/main.js
@@ -49,3 +49,48 @@ modalCart.addEventListener('click', function (event) {
 	}
 }
 
+// goods
+
+const more = document.querySelector('.more');
+const navigationItem = document.querySelectorAll('.navigation-item');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async function () {
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw 'Ошибка:' + result.status
+	}
+	return await result.json();
+};
+
+const creatCard = function (objCard) {
+	const card = document.createElement('div')
+	card.className = 'col-lg-3 col-sm-6'
+
+	card.innerHTML = `
+	<div class="goods-card">
+		<span class="label">New</span>
+		<img src="img/image-119.jpg" alt="image: Hoodie" class="goods-image">
+		<h3 class="goods-title">Embroidered Hoodie</h3>
+		<p class="goods-description">Yellow/Lilac/Fuchsia/Orange</p>
+		<button class="button goods-card-btn add-to-cart" data-id="007">
+		<span class="button-price">$89</span>
+		</button>
+	</div>
+	`;
+	console.log(card);
+	return card;
+}
+
+const renderCards = function (data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(creatCard)
+	document.body.classList.add('show-goods')
+};
+renderCards();
+
+
+
+// getGoods().then(function (data) {
+// 	console.log(data)
+// });
