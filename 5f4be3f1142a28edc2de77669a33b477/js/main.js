7a39c166d2e100bb267e50fed98b7b5e7b0fe70e
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2021-03-25 15:06:47 +0200

    HW2

--- 5f4be3f1142a28edc2de77669a33b477/js/main.js
+++ 5f4be3f1142a28edc2de77669a33b477/js/main.js
@@ -37,10 +37,10 @@ modalCart.addEventListener('click', function (event) {
 {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
-	for (let i = 0; i < scrollLinks.length; i++) {
-		scrollLinks[i].addEventListener('click', function (event) {
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', function (event) {
 			event.preventDefault();
-			const id = scrollLinks[i].getAttribute('href');
+			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
 				behavior: 'smooth',
 				block: 'start'
@@ -52,7 +52,7 @@ modalCart.addEventListener('click', function (event) {
 // goods
 
 const more = document.querySelector('.more');
-const navigationItem = document.querySelectorAll('.navigation-item');
+const navigationLink = document.querySelectorAll('.navigation-link');
 const longGoodsList = document.querySelector('.long-goods-list');
 
 const getGoods = async function () {
@@ -63,34 +63,55 @@ const getGoods = async function () {
 	return await result.json();
 };
 
-const creatCard = function (objCard) {
+const creatCard = function ({ label, name, img, description, id, price }) {
 	const card = document.createElement('div')
 	card.className = 'col-lg-3 col-sm-6'
 
+	
 	card.innerHTML = `
 	<div class="goods-card">
-		<span class="label">New</span>
-		<img src="img/image-119.jpg" alt="image: Hoodie" class="goods-image">
-		<h3 class="goods-title">Embroidered Hoodie</h3>
-		<p class="goods-description">Yellow/Lilac/Fuchsia/Orange</p>
-		<button class="button goods-card-btn add-to-cart" data-id="007">
-		<span class="button-price">$89</span>
+		${label ? `<span class="label">${label}</span>` : ''}
+		
+		<img src="db/${img}" alt="${name}" class="goods-image">
+		<h3 class="goods-title">${name}</h3>
+		<p class="goods-description">${description}</p>
+		<button class="button goods-card-btn add-to-cart" data-id="${id}">
+		<span class="button-price">$${price}</span>
 		</button>
 	</div>
 	`;
-	console.log(card);
+	
 	return card;
-}
-
-const renderCards = function (data) {
-	longGoodsList.textContent = '';
-	const cards = data.map(creatCard)
-	document.body.classList.add('show-goods')
 };
-renderCards();
 
+const renderCards = function(data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard);
+	longGoodsList.append(...cards);
+	document.body.classList.add('show-goods');
+};
 
+more.addEventListener('click', function(event) {
+	event.preventDefault();
+	getGoods().then(renderCards);
+});
 
-// getGoods().then(function (data) {
-// 	console.log(data)
-// });
+const filterCards = function (field, value) {
+	getGoods()
+		.then(function (data) {
+			const filteredGoods = data.filter(function (good) {
+				return good[field] === value
+			});
+			return filterCards;
+		})
+		.then(renderCards);
+};
+
+navigationLink.forEach(function (link) {
+	link.addEventListener('click', function (event) {
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		filterCards(field, value)
+	})
+});
