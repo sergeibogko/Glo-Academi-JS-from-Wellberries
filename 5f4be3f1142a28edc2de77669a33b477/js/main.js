a47679d2769bf8a7d5e1549edf97e19784c3c64f
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2021-03-26 18:15:04 +0200

    YW3

--- 5f4be3f1142a28edc2de77669a33b477/js/main.js
+++ 5f4be3f1142a28edc2de77669a33b477/js/main.js
@@ -12,12 +12,75 @@ const mySwiper = new Swiper('.swiper-container', {
 
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
+const longGoodsList = document.querySelector('.long-goods-list');
+const viewAll = document.querySelectorAll('.view-all');
+const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
+const showAcsessories = document.querySelectorAll('.show-acsessories');
+const showClothing = document.querySelectorAll('.show-clothing');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
 
-const openModal = function () {
+
+const getGoods = async () => {
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw 'Ошибочка вышла: ' + result.status
+	}
+	return await result.json();
+};
+
+const cart = {
+	cardGoods: [
+		{
+			id: "099",
+			name: "Часы",
+			price: 999,
+			count: 2,
+		},
+		{
+			id: "090",
+			name: "Кеды",
+			price: 9,
+			count: 3,
+		},
+	],
+	renderCard() {
+		cartTableGoods.textContent = '';
+		this.cartGoods.forEach(({ id, name, price, count }) => {
+			const trGood = document.createElement('tr');
+			trGood.className = 'cart-item';
+			trGood.dataset.id = id;
+
+			trGood.innerHTML = `
+				<td>${name}</td>
+				<td>${price}$</td>
+				<td><button class="cart-btn-minus">-</button></td>
+				<td>${count}</td>
+				<td><button class="cart-btn-plus">+</button></td>
+				<td>${price * count}$</td>
+				<td><button class="cart-btn-delete">x</button></td>
+			`;
+			cartTableGoods.append(trGood);
+		});
+		
+		const totaPrice = this.cardGoods.reduce((sum, item,) => {
+			return sum + item.price * item.count;
+		}, 0)
+		cartTableTotal.textContent = totalPrice + '$'
+	},
+	deletGood(id) { },
+	minusGood(id) { },
+	plusGood(id) { },
+	addCartGoods(id) {},
+}
+
+cart.renderCart()
+
+const openModal = () => {
 	modalCart.classList.add('show')
 }
 
-const closeModal = function () {
+const closeModal = () => {
 	modalCart.classList.remove('show')
 }
 
@@ -38,7 +101,7 @@ modalCart.addEventListener('click', function (event) {
 	const scrollLinks = document.querySelectorAll('a.scroll-link');
 
 	for (const scrollLink of scrollLinks) {
-		scrollLink.addEventListener('click', function (event) {
+		scrollLink.addEventListener('click', event => {
 			event.preventDefault();
 			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
@@ -51,52 +114,38 @@ modalCart.addEventListener('click', function (event) {
 
 // goods
 
-const longGoodsList = document.querySelector('.long-goods-list');
-const viewAll = document.querySelectorAll('.view-all');
-const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all)');
-const showAcsessories = document.querySelectorAll('.show-acsessories');
-const showClothing = document.querySelectorAll('.show-clothing');
-
-const getGoods = async function() {
-	const result = await fetch('db/db.json');
-	if (!result.ok) {
-		throw 'Ошибочка вышла: ' + result.status
-	}
-	return await result.json();
-};
-
 const creatCard = function ({ label, name, img, description, id, price }) {
-	const card = document.createElement('div')
-	card.className = 'col-lg-3 col-sm-6'
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
 
 	
 	card.innerHTML = `
-	<div class="goods-card">
-		${label ? `<span class="label">${label}</span>` : ''}
-		
-		<img src="db/${img}" alt="${name}" class="goods-image">
-		<h3 class="goods-title">${name}</h3>
-		<p class="goods-description">${description}</p>
-		<button class="button goods-card-btn add-to-cart" data-id="${id}">
-		<span class="button-price">$${price}</span>
-		</button>
-	</div>
+		<div class="goods-card">
+			${label ? `<span class="label">${label} </span>` :
+				''}
+			<img src="db/${img}" alt="${name}" class="goods-image">
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${id}">
+				<span class="button-price">$${price}</span>
+			</button>
+		</div>
 	`;
 	
 	return card;
 };
 
-const renderCards = function(data) {
+const renderCards = function (data) {
 	longGoodsList.textContent = '';
-	const cards = data.map(createCard);
-	longGoodsList.append(...cards);
-	document.body.classList.add('show-goods');
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards)
+	document.body.classList.add('show-goods')
 };
 
-const showAll = function(event) {
+const showAll = function (event) {
 	event.preventDefault();
 	getGoods().then(renderCards);
-}
+};
 
 viewAll.forEach(function (elem) {
 	elem.addEventListener('click', showAll);
@@ -104,17 +153,12 @@ viewAll.forEach(function (elem) {
 
 const filterCards = function (field, value) {
 	getGoods()
-		.then(function (data) {
-			const filteredGoods = data.filter(function (good) {
-				return good[field] === value
-			});
-			return filterCards;
-		})
+		.then(data => data.filter(good => good[field] === value))
 		.then(renderCards);
 };
 
 navigationLink.forEach(function (link) {
-	link.addEventListener('click', function (event) {
+	link.addEventListener('click', event => {
 		event.preventDefault();
 		const field = link.dataset.field;
 		const value = link.textContent;
